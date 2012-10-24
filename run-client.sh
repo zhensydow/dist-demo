08c81b3c56804577bcd8b34619337d7ff240d90f
Luis Cabellos <cabellos@ifca.unican.es>
2012-10-24 16:18:13 +0200

    remove unused message

--- run-client.sh
+++ run-client.sh
@@ -3,6 +3,4 @@
 FSTPORT=7766
 HOST=`hostname`
 
-echo RUN-CLIENT $HOST $SLURM_LOCALID $((SLURM_LOCALID + FSTPORT))
-
 ~/.cabal/bin/demo-worksteal slave $HOST $((SLURM_LOCALID + FSTPORT))
--- run-server.sh
+++ run-server.sh
@@ -3,6 +3,4 @@
 FSTPORT=7866
 HOST=`hostname`
 
-echo RUN-SERVER $HOST $SLURM_LOCALID $((SLURM_LOCALID + FSTPORT))
-
 ~/.cabal/bin/demo-worksteal master $HOST $((SLURM_LOCALID + FSTPORT)) 1000
