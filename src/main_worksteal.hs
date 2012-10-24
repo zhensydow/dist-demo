a769b7ca80fff91d2b52c7b1045933efa9112498
Luis Cabellos <cabellos@ifca.unican.es>
2012-10-24 13:20:22 +0200

    pretty printing of main

--- src/main_worksteal.hs
+++ src/main_worksteal.hs
@@ -4,12 +4,13 @@ module Main where
 -- -----------------------------------------------------------------------------
 import System.IO( BufferMode(..), hSetBuffering, stdout )
 import Control.Distributed.Process( RemoteTable )
-import Control.Distributed.Process.Node (initRemoteTable)
+import Control.Distributed.Process.Node( initRemoteTable )
 import Control.Distributed.Process.Backend.SimpleLocalnet( 
   initializeBackend, startSlave, startMaster )
 import Control.Monad.IO.Class( liftIO )
 import System.Environment( getArgs )
 import Demo.WorkStealing( __remoteTable, master )
+import Demo.Util( printSlaveResume )
 
 -- -----------------------------------------------------------------------------
 rtable :: RemoteTable
@@ -25,13 +26,14 @@ main = do
       putStrLn $ "START Master on: " ++ host ++ " " ++ port
       backend <- initializeBackend host port rtable 
       startMaster backend $ \slaves -> do
-        liftIO $ print slaves
+        liftIO $ printSlaveResume (" + [" ++ host ++ ":" ++ port ++ "]") slaves
         result <- master (read n) slaves
         liftIO $ print result 
       putStrLn "END Master"
     ["slave", host, port] -> do
       putStrLn $ "START slave on: " ++ host ++ " " ++ port
       backend <- initializeBackend host port rtable 
+      putStrLn $ " - [" ++ host ++ ":" ++ port ++ "] running..."
       startSlave backend
       putStrLn "END slave"
     _ -> do
