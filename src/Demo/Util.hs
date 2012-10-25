d4f1c392e2245e617cd2b48b876d4c6d2768f971
Luis Cabellos <cabellos@ifca.unican.es>
2012-10-25 11:11:59 +0200

    add node expand function in Util

--- src/Demo/Util.hs
+++ src/Demo/Util.hs
@@ -1,17 +1,22 @@
 -- -----------------------------------------------------------------------------
-module Demo.Util( printSlaveResume ) where
+module Demo.Util( printSlaveResume, nidNode, expandNodenames ) where
 
 -- -----------------------------------------------------------------------------
+import Control.Arrow( (&&&) )
 import Control.Monad( forM_ )
 import Control.Distributed.Process( NodeId )
 import Text.Regex.TDFA( (=~) )
-import Control.Arrow( (&&&) )
 import Data.List( sort, group )
+import qualified Data.Text as T( Text, pack, append )
+import Text.Parsec.Text( Parser )
+import Text.Parsec.Prim( (<|>), (<?>), try, parse )
+import Text.Parsec.Combinator( many1, sepBy1 )
+import Text.Parsec.Char( char, alphaNum, digit )
 
 -- -----------------------------------------------------------------------------
-readNID :: NodeId -> String
-readNID nid = case regres of
-  [(_:nn:_)] -> nn
+nidNode :: NodeId -> String
+nidNode nid = case regres of
+  [_:nn:_] -> nn
   _ -> "*"
   where
     regres = show nid =~ "nid://(.+):(.+):(.+)" :: [[String]]
@@ -20,13 +25,71 @@ readNID nid = case regres of
 rle :: [NodeId] -> [(Int, String)]
 rle slaves = map (length &&& head) sorted
   where
-    sorted = group . sort . map readNID $ slaves
-  
+    sorted = group . sort . map nidNode $ slaves
+
 -- -----------------------------------------------------------------------------
 printSlaveResume :: String -> [NodeId] -> IO ()
 printSlaveResume pre slaves = do
   putStrLn $ pre ++ " " ++ (show . length $ slaves) ++ " slave/s"
-  forM_ (rle slaves) $ \(n,name) -> do
+  forM_ (rle slaves) $ \(n,name) ->
     putStrLn $ pre ++ " ->" ++ name ++ " = " ++ show n
-  
+
+-- -----------------------------------------------------------------------------
+expandNodenames :: T.Text -> [T.Text]
+expandNodenames def = case parse nodeList "" def of
+  Left err -> error $ show err
+  Right x -> x
+
+-- -----------------------------------------------------------------------------
+nodeList :: Parser [T.Text]
+nodeList = do {
+                  l <- sepBy1 nodes $ char ',';
+                  return $ concat l
+                } <?> "node list"
+
+-- -----------------------------------------------------------------------------
+nodes :: Parser [T.Text]
+nodes = do {
+             pre <- nodePrefix;
+             l <- intList <|> return [];
+             if null l
+               then return [pre]
+               else return $ map (T.append pre . T.pack . show) l
+           } <?> "nodes"
+
+-- -----------------------------------------------------------------------------
+nodePrefix :: Parser T.Text
+nodePrefix = fmap T.pack $ many1 alphaNum
+
+-- -----------------------------------------------------------------------------
+intList :: Parser [Integer]
+intList = do {
+          _ <- char '[';
+          l <- sepBy1 intSequence $ char ',';
+          _ <- char ']';
+          return . sort $ concat l
+        } <?> "intList"
+
+-- -----------------------------------------------------------------------------
+intSequence :: Parser [Integer]
+intSequence = try (
+                 do {
+                   a <- number;
+                   _ <- char '-';
+                   b <- number;
+                   return [a..b]
+                 })
+            <|>
+            do {
+              n <- number;
+              return [n]
+            } <?> "intSequence"
+
+-- -----------------------------------------------------------------------------
+number :: Parser Integer
+number = do {
+           ds <- many1 digit;
+           return $ read ds
+         } <?> "number"
+
 -- -----------------------------------------------------------------------------
